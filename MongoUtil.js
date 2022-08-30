dcb12f0aea822b19797c9284e419f4a3262b6318
Fritz Latagan <gerald.latagan@gmail.com>
2022-08-30 14:11:07 +0800

    Created MongoUtil.js
    this will help us connect to mongodb

---
+++ MongoUtil.js
@@ -0,0 +1,11 @@
+const MongoClient = require('mongodb').MongoClient;
+
+async function connect(mongoUri, databaseName) {
+    const client = await MongoClient.connect(mongoUri, {
+        "useUnifiedTopology": true
+    })
+    const db = client.db(databaseName);
+    return db; 
+}
+
+module.exports = {connect};
